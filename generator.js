1ecd3b0f1786c51d0150b1a0e2934076c4d95266
Shurena <shurena89@gmail.com>
2022-10-06 15:13:17 +0200

    final

--- generator.js
+++ generator.js
@@ -1,18 +1,33 @@
+
+
 let answer = document.querySelector('.ask');
 
 let picture = document.querySelector('.img-main');
 
+
 let comment = document.querySelector('.comment');
+
+let fon = document.querySelector('body');
 
 
 
 answer.addEventListener('click', function(){
     let random = Math.round(Math.random());
     let proceed = " Click the button again if you have another question.";
+    picture.setAttribute("class", "pic_size");
+    
     if(random<=0.5){
-    comment.innerText = "The answer in YES!" + proceed
+    comment.innerText = "The answer in YES!" + proceed;
+    picture.src = "./images/small_yes.png";
+    fon.style.backgroundColor = "#dd79ccad";
+    
+   
 }else{
-comment.innerText = "The answer in NO!" + proceed
+comment.innerText = "The answer in NO!" + proceed;
+picture.src = "./images/small_no.png";
+
+fon.style.backgroundColor = "#cadeff";
+
 }
 
 })
